3fd3f6a5a4ee0c15fa03d02d176d0bb7b4a803b1
ryan p <conversationtheory@gmail.com>
2012-12-05 00:09:59 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pressplay::Application.config.secret_token = '3eb64ff1142da010e7280dc6518b7c47ce6bff7d21326efe17821bad60e5c28d56f0236eb50b0621da605192158e74374f3dd21bbe066c9a9ba8a8d764797f79'
